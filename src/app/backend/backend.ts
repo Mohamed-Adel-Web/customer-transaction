bfd89f3841c842879831d6869d1815dc6dd242bd
unknown <mohamedadeladlymohamed@gmail.com>
2024-07-12 15:31:30 +0300

    v-1.1

--- src/app/backend/backend.ts
+++ src/app/backend/backend.ts
@@ -1,3 +1,3 @@
-const baseUrl = `http://localhost:3000`;
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 export const customersUrl= `${baseUrl}/customers`
 export const transactionUrl= `${baseUrl}/transaction`
